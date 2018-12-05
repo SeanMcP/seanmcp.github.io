31ee2c0d01edbfa6fc29250926b7d654579c50f8
Sean McPherson <seanmcp25@gmail.com>
2018-12-05 06:44:49 -0500

    Troubleshoot

--- src/components/post/PostHelmet.js
+++ src/components/post/PostHelmet.js
@@ -13,10 +13,10 @@ const PostHelmet = ({
         meta={[{ name: 'description', content: postDescription }]}
         title={`${postTitle} | ${siteTitle}`}
     >
-        <meta name="twitter:card" content={postDescription} />
-        <meta name="twitter:site" content="@_seanmcp" />
+        <meta name="twitter:card" content={'summary'} />
+        <meta name="twitter:site" content={'@_seanmcp'} />
         <meta name="twitter:title" content={postTitle} />
-        {/* <meta name="twitter:description" content="View the album on Flickr." /> */}
+        <meta name="twitter:description" content={postDescription} />
         {postImageUrl && <meta name="twitter:image" content={postImageUrl} />}
     </Helmet>
 );
